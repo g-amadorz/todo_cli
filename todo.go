335db48a532b23c3e080fc7c5934c785ef1c7786
Gabe <gabe.amadorz@gmail.com>
2025-08-17 11:50:58 -0700

    Set up basic todo API with add, complete and delete methods.

--- todo.go
+++ todo.go
@@ -27,9 +27,27 @@ func (l *TodoList) Add(taskName string) {
 	*l = append(*l, todo)
 }
 
-func (l *TodoList) Complete(todo int) {
+func (l *TodoList) Complete(todo int) error {
 	if len(*l) < todo || todo < 1 {
 		fmt.Errorf("Todo: %d was unable to be completed", todo)
 	}
 
+	ls := *l
+
+	ls[todo-1].Completed = true
+	ls[todo-1].CompletedAt = time.Now()
+
+	return nil
+}
+
+func (l *TodoList) Delete(todo int) error {
+	if len(*l) < todo || todo < 1 {
+		fmt.Errorf("Todo: %d was unable to be deleted", todo)
+	}
+
+	ls := *l
+
+	*l = append(ls[:todo-1], ls[todo:]...)
+
+	return nil
 }
